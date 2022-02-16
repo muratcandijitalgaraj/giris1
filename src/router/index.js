dfc49ccd676e8ea13e6256f142b059d43d3cc370
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-16 11:13:08 +0300

    add kayıt part without the date picker

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Giris1.vue"),
   },
+  {
+    path: "/kayit",
+    name: "Kayit",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Kayit.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/Kayit.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="forBackgroundColor">
+    <div class="kayitComponent">
+      <Header para="Üye misiniz?" buttonText="Giriş Yap" />
+      <Bulutlar />
+      <MainPart />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import Bulutlar from "../components/kayit1/Bulutlar.vue";
+import MainPart from "../components/kayit1/MainPart.vue";
+
+export default {
+  components: { Header, Bulutlar, MainPart },
+};
+</script>
+
+<style scoped lang="scss">
+$backgroundColor: #e9f3f9;
+.forBackgroundColor {
+  width: 100%;
+  height: 100%;
+  background: $backgroundColor;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+.kayitComponent {
+  width: 100%;
+  //will change this later on
+  height: 500px;
+  background: linear-gradient(
+      0deg,
+      #e9f3f9 12.23%,
+      rgba(233, 243, 249, 0) 77.17%
+    ),
+    url("../assets/giris/bulutlar.svg");
+  background-repeat: no-repeat;
+}
+</style>
